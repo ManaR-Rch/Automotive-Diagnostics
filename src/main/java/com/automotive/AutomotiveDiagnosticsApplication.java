8d35916311b3cae26b49e6dfa690b0f492de34b7
ManaR-Rch <manarmarchou6@gmail.com>
2025-12-03 12:02:20 +0100

    Setup project

---
+++ src/main/java/com/automotive/AutomotiveDiagnosticsApplication.java
@@ -0,0 +1,13 @@
+package com.automotive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomotiveDiagnosticsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AutomotiveDiagnosticsApplication.class, args);
+  }
+
+}
