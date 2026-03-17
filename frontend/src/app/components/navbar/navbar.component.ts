721f3997bc805803a88f438bc3500a3d48276f61
ManaR-Rch <manarmarchou6@gmail.com>
2026-03-17 11:53:18 +0000

    chore: micro-commit 9/100 - frontend/src/app/components/navbar/navbar.component.ts

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectCurrentUser, selectIsAuthenticated, selectIsAdmin } from '../../store/auth/auth.selectors';
+import { AuthActions } from '../../store/auth/auth.actions';
+import { User } from '../../core/models/user.model';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isAuthenticated$: Observable<boolean>;
+  currentUser$: Observable<User | null>;
+  isAdmin$: Observable<boolean>;
+  mobileOpen = false;
+  userMenuOpen = false;
 
+  constructor(private store: Store) {
+    this.isAuthenticated$ = this.store.select(selectIsAuthenticated);
+    this.currentUser$     = this.store.select(selectCurrentUser);
+    this.isAdmin$         = this.store.select(selectIsAdmin);
+  }
+
+  ngOnInit() {}
+
+  logout() {
+    this.store.dispatch(AuthActions.logout());
+  }
+
+  toggleMobile() { this.mobileOpen = !this.mobileOpen; }
+  toggleUserMenu() { this.userMenuOpen = !this.userMenuOpen; }
 }
